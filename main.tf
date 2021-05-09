69a3a9198df670ea6bb649a4806010644fe34239
master4646 <80125880+master4646@users.noreply.github.com>
2021-05-09 17:22:17 +0300

    Initial Commit
    Creates a VPC

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+output "vpcid" {
+    description = "The Subnet ID"
+    value = aws_vpc.main.id
+}
